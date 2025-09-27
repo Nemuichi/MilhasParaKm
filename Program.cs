c9b7faa832d1db8c4807b2d9caf62e0ba80ec999
Thyago Ladislau <thyagothyago600@gmail.com>
2025-09-26 22:45:27 -0300

    Programa concluido

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double milhas, km;
+
+milhas = 1.609;
+
+Console.WriteLine("Milhas para KM");
+Console.Write("Informe o valor em milhas: ");
+
+double valorColocado = Convert.ToDouble(Console.ReadLine()!);
+km = valorColocado * milhas;
+
+Console.WriteLine($"{milhas} milhas em km --> {km}");
+
+
+
+
+
+
+
+
+
+
